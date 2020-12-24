85f7bdee94c85c0207e612ccbdf26a2236d7c508
quantum rose <cui-haojie@outlook.com>
2020-12-24 17:33:33 +0800

    添加一些说明

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="home">
         <h1>vue-component</h1>
+        <p>
+            一些方便在Vue项目中使用的组件。目前已经完成的有：序列帧、刮刮卡。
+            <a href="https://github.com/quantum-rose/vue-component">Github</a>，
+            <a href="https://gitee.com/quantum-rose/vue-component">码云</a>。
+        </p>
     </div>
 </template>
 
@@ -12,7 +17,15 @@ export default {
 
 <style lang="scss" scoped>
 .home {
-    text-align: center;
+    padding: 15px;
     color: #2c3e50;
+
+    h1 {
+        text-align: center;
+    }
+
+    p {
+        text-indent: 2em;
+    }
 }
 </style>
